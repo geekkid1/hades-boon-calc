a9f9fca6e09dbe6349c0954e2ef726c6b039a1aa
Emily <captain.emily.blum@gmail.com>
2021-11-21 22:10:06 -0500

    Proof of concept interactivity added to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class FakeUndoButton extends React.Component {
+  render() {
+  return (
+    <button onClick={this.props.onClick}>Undo</button>
+  );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onUndoClick = this.onUndoClick.bind(this);
+    this.state = {
+      undone: false,
+    };
+  }
+
+  onUndoClick() {
+    this.setState({undone: true});
+  }
+
+  render() {
+    const undone = this.state.undone;
+    let text;
+    if(undone) {
+      text = <p>Edit <code>src/App.js</code> and save to reload.</p>
+    } else {
+      text = <p>I edited <code>src/App.js</code> successfully!</p>
+    }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I edited <code>src/App.js</code> successfully!
-        </p>
+        {text}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +44,11 @@ function App() {
         >
           Learn React
         </a>
+        <FakeUndoButton onClick={this.onUndoClick} />
       </header>
     </div>
   );
+  }
 }
 
 export default App;
