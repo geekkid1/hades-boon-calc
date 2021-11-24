db65ededcb69e91a6c5b6efad8f01d35f54a2da1
Emily <captain.emily.blum@gmail.com>
2021-11-24 11:36:12 -0500

    Add boons.js and add a single boon to demonstrate the numbers. Text will be added soon.

---
+++ src/boons.js
@@ -0,0 +1,16 @@
+export var boons = [
+    {
+        id: "zvengeance",
+        name: "Heaven's Vengeance",
+        god: "Zeus",
+        type: "revenge",
+        duo: false,
+        legendary: false,
+        progression : {
+            common: 80,
+            rare: [104,120],
+            epic: [144,160],
+            heroic: [184,200]
+        }
+    }
+];
