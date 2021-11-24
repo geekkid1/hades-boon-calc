a9bfda7288323497284f29d995fca24f005719f2
Emily <captain.emily.blum@gmail.com>
2021-11-24 11:41:21 -0500

    Add text section, type text section, remove strict typing and rename progression to damageProgression. Schema subject to extreme change

--- src/boons.js
+++ src/boons.js
@@ -3,14 +3,16 @@ export var boons = [
         id: "zvengeance",
         name: "Heaven's Vengeance",
         god: "Zeus",
-        type: "revenge",
         duo: false,
         legendary: false,
-        progression : {
+        value : {
             common: 80,
             rare: [104,120],
             epic: [144,160],
             heroic: [184,200]
+        },
+        text: {
+            revenge: "<b>Lightning</b> strikes foes around you, dealing <b>{value} damage</b>."
         }
     }
 ];
